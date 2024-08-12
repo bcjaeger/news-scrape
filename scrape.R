f6ede90b5e4675b26d4b4b994e82088e20febcaa
bcjaeger <byron.jaeger@gmail.com>
2024-08-12 18:35:10 -0400

    jsonlite

--- scrape.R
+++ scrape.R
@@ -1,6 +1,7 @@
 library(httr2)
 
-cat("The key is ", Sys.getenv("NEWS_API_KEY"), file = stderr())
+cat("The key is ", Sys.getenv("NEWS_API_KEY"), "\n",
+    file = stderr())
 
 req <- request("https://newsapi.org/v2/everything") |>
   req_url_query(
